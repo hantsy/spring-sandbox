11a4efee5cb91c267f85ea5cdcdf6ae0da0cf1cb
hantsy <hantsy@gmail.com>
2013-10-31 23:02:14 +0800

    added jpa auditing example.

---
+++ jpa-audit/src/main/java/com/hantsylabs/example/spring/model/AuditListener.java
@@ -0,0 +1,31 @@
+package com.hantsylabs.example.spring.model;
+
+import java.util.Date;
+
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+public class AuditListener {
+	@PrePersist
+	public void prePersist(Object o) {
+		final Date _createdDate = new Date();
+
+		if (o instanceof Signup) {
+			Signup signup = (Signup) o;
+			signup.setCreatedDate(_createdDate);
+			signup.setModifiedDate(_createdDate);
+			signup.setCreatedBy(SecurityUtils.getCurrentUser());
+			signup.setModifiedBy(SecurityUtils.getCurrentUser());
+		}
+
+	}
+
+	@PreUpdate
+	public void preUpdate(Object o) {
+		if (o instanceof Signup) {
+			Signup signup = (Signup) o;
+			signup.setModifiedDate(new Date());
+			signup.setModifiedBy(SecurityUtils.getCurrentUser());
+		}
+	}
+}
